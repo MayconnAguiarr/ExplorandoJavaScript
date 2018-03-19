0dee7189611db94d7b4a824a402dada9d0fa1285
Maycon <mayconn.aguiarr@gmail.com>
2018-03-19 16:29:06 -0300

    alterado README e arquivo VARIAVEIS_E_HOISTING

--- variaveis_e_hoisting.js
+++ variaveis_e_hoisting.js
@@ -1,6 +1,34 @@
 var percentualImposto = 0.15;
 var totalImposto = valor * percentualImposto;
 
-console.log('Total de Imposto a pagar ', totalImposto);
+console.log('Total imposto a pagar', totalImposto);
 
 var valor = 100;
+
+// HOISTING
+/*
+var percentualImposto, totalImposto, valor;
+percentualImposto = 0.15;
+totalImposto = valor * percentualImposto;
+console.log('Total imposto a pagar', totalImposto);
+valor = 100;
+*/
+
+//******************************************************************************************************
+
+var percentualImposto = 0.15;
+valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto);
+
+var valor;
+
+// HOISTING
+/*
+var percentualImposto, valor, totalImposto;
+percentualImposto = 0.15;
+valor = 100;
+totalImposto = valor * percentualImposto;
+console.log('Total imposto a pagar', totalImposto);
+*/
