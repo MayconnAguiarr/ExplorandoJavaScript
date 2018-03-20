3eb97a622cebcba0c8ac56f68ce55178201c4ba9
Maycon <mayconn.aguiarr@gmail.com>
2018-03-19 23:52:16 -0300

    adicionado exemplo valoresNullEundefined

---
+++ valoresNulleundefined.js
@@ -0,0 +1,17 @@
+console.log(null === undefined);
+
+console.log(null == undefined)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+
+var x = null;
+console.log(x);
+
+if (x != null) {
+	console.log('Existe um valor em x:', x);
+} else {
+	console.log('x não tem um valor útil');
+}
