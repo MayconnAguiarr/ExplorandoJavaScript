9ba152c8b9b5a339f0199d43d1c7a297502f6b06
Maycon <mayconn.aguiarr@gmail.com>
2018-03-20 01:12:06 -0300

    add 'closures.js'

---
+++ funcoes/closure.js
@@ -0,0 +1,13 @@
+// váriavel valor encapsulada
+
+var incrementar = (function() {
+	var valor = 0;
+
+	return function() {
+		return ++valor;
+	}
+})();
+
+console.log(incrementar());
+console.log(incrementar());
+console.log(incrementar());
