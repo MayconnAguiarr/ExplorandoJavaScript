2bc948f39ea9404102de9f9b7c4329907cb686d3
Maycon <mayconn.aguiarr@gmail.com>
2018-03-20 22:22:11 -0300

    add exercício 'diferencaArrayEobjetos.js'

---
+++ arrays/diferencaArrayEobjetos.js
@@ -0,0 +1,31 @@
+var foxArray = ['Fox', 'Prata', 'VW'];
+
+typeof(foxArray);
+
+console.log(foxArray[0]);
+
+console.log(foxArray[1]);
+
+console.log(foxArray[2]);
+
+//**********************************
+
+var foxObject = {modelo:'Fox', cor: 'Prata', fabricante: 'VW'}
+
+typeof(foxObject);
+
+console.log('-------------------')
+
+console.log(foxObject.modelo);
+
+console.log(foxObject.cor);
+
+console.log(foxObject.fabricante);
+
+
+console.log('-------------------')
+
+console.log(foxArray);
+
+console.log(foxObject);
+
