2b304148045e5df8c9304f49e214db5a136596c2
Maycon <mayconn.aguiarr@gmail.com>
2018-03-20 22:34:25 -0300

    add exercício 'adicionarEremoverElementosNoArray.js'

---
+++ arrays/adicionarEremoveElementosNoArray.js
@@ -0,0 +1,28 @@
+var emails = [];
+
+// Adicionando elementos
+
+emails.push('Maycon@gmail.com');
+
+console.log(emails);
+
+emails.push('Amanda@hotmail.com');
+
+console.log(emails);
+
+//adicionar no inicio do array
+emails.unshift('Marlene@hotmail.com');
+
+console.log(emails);
+
+
+// Removendo elementos
+
+emails.pop();
+
+console.log(emails);
+
+emails.shift();
+
+console.log(emails);
+
